0a4c57223a5bad4ab0c26d5c5d4a757f2ef2db63
colespen <spencerlewiscole@gmail.com>
2023-03-05 12:19:16 -0500

    handle post and on message events for getData worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { processList } from "./longProcesses/enums";
+
+import Loader from "./components/Loader";
 
 type LengthCountType = {
   loading: boolean;
@@ -29,9 +31,15 @@ export type GetDataType = {
 export const listPageSize = 50;
 
 const App = () => {
+
   const [lengthCount, setLengthCount] = useState<LengthCountType>({
     loading: true,
     value: 0,
+  });
+  const [profileList, setProfileList] = useState<ProfileListType>({
+    loading: true,
+    list: [],
+    page: 1,
   });
 
   // initialize new web worker
@@ -40,6 +48,11 @@ const App = () => {
     // pass in new instance of generated URL that contains path to count worker file --- import.meta.url:
     // returns URL of current module's file and ensures that count.ts is loaded relative to current module's file location
     () => new Worker(new URL("./longProcesses/count.ts", import.meta.url)),
+    []
+  );
+
+  const getData: Worker = useMemo(
+    () => new Worker(new URL("./longProcesses/getData.ts", import.meta.url)),
     []
   );
 
@@ -66,6 +79,36 @@ const App = () => {
       };
     }
   }, [counter]);
+
+  // postMessage - send to getData worker on inital render
+  useEffect(() => {
+    if (window.Worker) {
+      const request = {
+        action: processList.getData,
+        period: "initial",
+        thePageNumber: profileList.page,
+      } as GetDataType;
+
+      getData.postMessage(JSON.stringify(request));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // onmessage - recieve from getData worker
+  useEffect(() => {
+    if (window.Worker) {
+      getData.onmessage = (e: MessageEvent<string>) => {
+        const response = JSON.parse(e.data) as unknown as ProfileListType;
+
+        setProfileList((prev) => ({
+          ...prev,
+          loading: response.loading,
+          list: response.list,
+          page: response.page,
+        }));
+      };
+    }
+  }, [getData]);
 
   return (
     <main className="main-container">
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { ProfileType } from "../App";
 import { ProfileEnum } from "../longProcesses/enums";
 
+// define PropType
 type Props = {
   list: Array<ProfileType>;
 };
